c4837a7213915699397193b691e5a9a2476ed63a
Alan McBee <alan.mcbee@microsoft.com>
2023-04-12 14:13:32 -0700

    Use #requires with an imported module

--- PowerShell Modules/requires module/simpleScript.Tests.ps1
+++ PowerShell Modules/requires module/simpleScript.Tests.ps1
@@ -10,4 +10,14 @@ Context 'PS1 with no param block and a #requires -Modules statement' {
             } | Should -Throw -PassThru | Write-Host
         }
     }
+
+    Describe 'simpleScriptExpectingImportedModule.ps1' {
+        It 'successfully uses a required module' {
+            
+            Import-Module "$PSScriptRoot\simpleModule.psm1"
+
+            $out = & "$PSScriptRoot\simpleScriptExpectingImportedModule.ps1"
+            $out | Should -Be 'OK'
+        }
+    }
 }
---
+++ PowerShell Modules/requires module/simpleScriptExpectingImportedModule.ps1
@@ -0,0 +1,7 @@
+# NOTE: The simpleModule module must be imported in order for this script to be loaded and run
+
+#requires -Modules simpleModule
+
+$out = Test-Function
+
+Write-Output $out
